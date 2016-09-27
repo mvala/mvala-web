a082c905949072d8115095913e9fdcabfc759cc7
Martin Vala <mvala@saske.sk>
2016-09-27 11:27:46 +0200

    Server info component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,12 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ServerInfoService } from './shared/server-info.service';
+import { Component} from '@angular/core';
+import { ServerInfoComponent } from './server/server-info/server-info.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [ServerInfoService]
+  styleUrls: ['./app.component.css']
 })
 
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent {
   title = 'app works!';
-  connection: any;
-  info: any;
-
-  constructor(private appInfoService: ServerInfoService) {
-    this.info = { version: '', user_count: 0 };
-  }
-
-  ngOnInit() {
-    this.connection = this.appInfoService.getInfo().subscribe(info => {
-      this.info = info;
-    });
-  }
-
-  ngOnDestroy() {
-    this.connection.unsubscribe();
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ServerInfoComponent } from './server/server-info/server-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerInfoComponent
   ],
   imports: [
     BrowserModule,
